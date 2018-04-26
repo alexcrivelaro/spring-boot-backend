492782552f185fe5ac58889543317485708b8b24
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2018-04-26 16:53:59 -0300

    Projeto criado

---
+++ src/main/java/com/fonseca/springmodconc/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.fonseca.springmodconc;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringModConcApplication.class);
+	}
+
+}
---
+++ src/main/java/com/fonseca/springmodconc/SpringModConcApplication.java
@@ -0,0 +1,12 @@
+package com.fonseca.springmodconc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModConcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModConcApplication.class, args);
+	}
+}

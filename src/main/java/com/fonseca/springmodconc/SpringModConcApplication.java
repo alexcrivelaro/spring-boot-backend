2cf6a7d25438bac8a208db21a14897828ec1f914
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2018-04-30 17:58:49 -0300

    Criando operacao de instanciacao

--- src/main/java/com/fonseca/springmodconc/SpringModConcApplication.java
+++ src/main/java/com/fonseca/springmodconc/SpringModConcApplication.java
@@ -1,12 +1,32 @@
 package com.fonseca.springmodconc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fonseca.springmodconc.domain.Categoria;
+import com.fonseca.springmodconc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringModConcApplication {
+public class SpringModConcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringModConcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		
 	}
 }

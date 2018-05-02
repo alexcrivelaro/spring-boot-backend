06a85ac3a2e898ed8103944d143d60e53dd4b91c
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2018-05-02 18:55:21 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/fonseca/springmodconc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.fonseca.springmodconc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fonseca.springmodconc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}

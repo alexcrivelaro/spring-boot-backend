9b9670e45eea9e76d01f7f07fcc6a5ec814c4b89
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2018-05-07 08:32:26 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/fonseca/springmodconc/dto/ClienteDTO.java
+++ src/main/java/com/fonseca/springmodconc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.fonseca.springmodconc.domain.Cliente;
+import com.fonseca.springmodconc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
